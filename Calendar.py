05010880637f08d1c2c3c98995cf8b07bfb92313
vanthao43st <nvthao43.hnvn@gmail.com>
2025-11-28 17:16:22 +0700

    306_optimizer.ipynb

---
+++ Calendar.py
@@ -0,0 +1,10 @@
+from calendar import *
+
+# year = int(input("Enter year: "))
+year = 2025
+print(calendar(year, 2, 1, 8, 6))
+
+#2 = 2 characters for days (Mo, Tu, etc.)
+#1 = 1 space between rows
+#8 = 8 rows for each month
+#3 = 3 columns for all months of the year
